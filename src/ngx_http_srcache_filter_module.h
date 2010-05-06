ec8039896ad13663f4b763f562e7c089188d3124
agentzh (章亦春) <agentzh@gmail.com>
2010-05-06 14:56:07 +0800

    initial checkin.

---
+++ src/ngx_http_srcache_filter_module.h
@@ -0,0 +1,31 @@
+#ifndef NGX_HTTP_SRCACHE_FILTER_MODULE_H
+#define NGX_HTTP_SRCACHE_FILTER_MODULE_H
+
+
+#include <ngx_core.h>
+
+
+typedef struct {
+    ngx_int_t                   method;
+    ngx_http_complex_value_t    location;
+    ngx_http_complex_value_t    args;
+} ngx_http_srcache_request_t;
+
+typedef struct {
+    ngx_http_srcache_request_t      *fetch;
+    ngx_http_srcache_request_t      *store;
+
+    size_t        buf_size;
+} ngx_http_srcache_conf_t;
+
+typedef struct {
+    ngx_flag_t      waiting_subrequest:1;
+    ngx_flag_t      request_done:1;
+    ngx_flag_t      from_cache:1;
+    ngx_flag_t      in_subrequest:1;
+    ngx_flag_t      ignore_body:1;
+} ngx_http_srcache_ctx_t;
+
+
+#endif
+
---
+++ util/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# this file is mostly meant to be used by the author himself.
+
+root=`pwd`
+cd ~/work
+version=$1
+home=~
+opts=$2
+lwp-mirror "http://sysoev.ru/nginx/nginx-$version.tar.gz" nginx-$version.tar.gz
+tar -xzvf nginx-$version.tar.gz
+cd nginx-$version/
+if [[ "$BUILD_CLEAN" -eq 1 || ! -f Makefile || "$root/config" -nt Makefile || "$root/util/build.sh" -nt Makefile ]]; then
+    ./configure --prefix=/opt/nginx \
+          --add-module=$root/../echo-nginx-module \
+          --add-module=$root $opts \
+          --with-debug
+          #--add-module=$home/work/ngx_http_auth_request-0.1 #\
+          #--with-rtsig_module
+          #--with-cc-opt="-g3 -O0"
+          #--add-module=$root/../echo-nginx-module \
+  #--without-http_ssi_module  # we cannot disable ssi because echo_location_async depends on it (i dunno why?!)
+
+fi
+if [ -f /opt/nginx/sbin/nginx ]; then
+    rm -f /opt/nginx/sbin/nginx
+fi
+if [ -f /opt/nginx/logs/nginx.pid ]; then
+    kill `cat /opt/nginx/logs/nginx.pid`
+fi
+make -j3
+make install
+
