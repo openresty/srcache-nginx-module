f10c0446c7cc6b717d39313b52329ddb5daecc00
agentzh (章亦春) <agentzh@gmail.com>
2010-05-09 22:10:40 +0800

    added util.h and util.c

---
+++ src/ngx_http_srcache_util.c
@@ -0,0 +1,221 @@
+#define DDEBUG 0
+#include "ddebug.h"
+
+#include "ngx_http_srcache_util.h"
+
+
+ngx_str_t  ngx_http_srcache_content_length_header_key = ngx_string("Content-Length");
+
+ngx_str_t  ngx_http_srcache_get_method = ngx_http_srcache_method_name("GET");
+ngx_str_t  ngx_http_srcache_put_method = ngx_http_srcache_method_name("PUT");
+ngx_str_t  ngx_http_srcache_post_method = ngx_http_srcache_method_name("POST");
+ngx_str_t  ngx_http_srcache_head_method = ngx_http_srcache_method_name("HEAD");
+ngx_str_t  ngx_http_srcache_copy_method = ngx_http_srcache_method_name("COPY");
+ngx_str_t  ngx_http_srcache_move_method = ngx_http_srcache_method_name("MOVE");
+ngx_str_t  ngx_http_srcache_lock_method = ngx_http_srcache_method_name("LOCK");
+ngx_str_t  ngx_http_srcache_mkcol_method = ngx_http_srcache_method_name("MKCOL");
+ngx_str_t  ngx_http_srcache_trace_method = ngx_http_srcache_method_name("TRACE");
+ngx_str_t  ngx_http_srcache_delete_method = ngx_http_srcache_method_name("DELETE");
+ngx_str_t  ngx_http_srcache_unlock_method = ngx_http_srcache_method_name("UNLOCK");
+ngx_str_t  ngx_http_srcache_options_method = ngx_http_srcache_method_name("OPTIONS");
+ngx_str_t  ngx_http_srcache_propfind_method = ngx_http_srcache_method_name("PROPFIND");
+ngx_str_t  ngx_http_srcache_proppatch_method = ngx_http_srcache_method_name("PROPPATCH");
+
+
+void
+ngx_http_srcache_discard_bufs(ngx_pool_t *pool, ngx_chain_t *in)
+{
+    ngx_chain_t         *cl;
+
+    for (cl = in; cl; cl = cl->next) {
+        if (cl->buf->temporary && cl->buf->memory
+                && ngx_buf_size(cl->buf) > 0) {
+            ngx_pfree(pool, cl->buf->start);
+        }
+
+        cl->buf->pos = cl->buf->last;
+    }
+}
+
+
+ngx_int_t
+ngx_http_srcache_parse_method_name(ngx_str_t **method_name_ptr)
+{
+    const ngx_str_t* method_name = *method_name_ptr;
+
+    switch (method_name->len) {
+    case 3:
+        if (ngx_str3cmp(method_name->data, 'G', 'E', 'T')) {
+            *method_name_ptr = &ngx_http_srcache_get_method;
+            return NGX_HTTP_GET;
+            break;
+        }
+
+        if (ngx_str3cmp(method_name->data, 'P', 'U', 'T')) {
+            *method_name_ptr = &ngx_http_srcache_put_method;
+            return NGX_HTTP_PUT;
+            break;
+        }
+
+        return NGX_HTTP_UNKNOWN;
+        break;
+
+    case 4:
+        if (ngx_str4cmp(method_name->data, 'P', 'O', 'S', 'T')) {
+            *method_name_ptr = &ngx_http_srcache_post_method;
+            return NGX_HTTP_POST;
+            break;
+        }
+        if (ngx_str4cmp(method_name->data, 'H', 'E', 'A', 'D')) {
+            *method_name_ptr = &ngx_http_srcache_head_method;
+            return NGX_HTTP_HEAD;
+            break;
+        }
+        if (ngx_str4cmp(method_name->data, 'C', 'O', 'P', 'Y')) {
+            *method_name_ptr = &ngx_http_srcache_copy_method;
+            return NGX_HTTP_COPY;
+            break;
+        }
+        if (ngx_str4cmp(method_name->data, 'M', 'O', 'V', 'E')) {
+            *method_name_ptr = &ngx_http_srcache_move_method;
+            return NGX_HTTP_MOVE;
+            break;
+        }
+        if (ngx_str4cmp(method_name->data, 'L', 'O', 'C', 'K')) {
+            *method_name_ptr = &ngx_http_srcache_lock_method;
+            return NGX_HTTP_LOCK;
+            break;
+        }
+        return NGX_HTTP_UNKNOWN;
+        break;
+
+    case 5:
+        if (ngx_strncmp("MKCOL", method_name->data, method_name->len) == 0) {
+            *method_name_ptr = &ngx_http_srcache_mkcol_method;
+            return NGX_HTTP_MKCOL;
+            break;
+        }
+        if (ngx_strncmp("TRACE", method_name->data, method_name->len) == 0) {
+            *method_name_ptr = &ngx_http_srcache_trace_method;
+            return NGX_HTTP_TRACE;
+            break;
+        }
+        return NGX_HTTP_UNKNOWN;
+        break;
+
+    case 6:
+        if (ngx_str6cmp(method_name->data, 'D', 'E', 'L', 'E', 'T', 'E')) {
+            *method_name_ptr = &ngx_http_srcache_delete_method;
+            return NGX_HTTP_DELETE;
+            break;
+        }
+
+        if (ngx_str6cmp(method_name->data, 'U', 'N', 'L', 'O', 'C', 'K')) {
+            *method_name_ptr = &ngx_http_srcache_unlock_method;
+            return NGX_HTTP_UNLOCK;
+            break;
+        }
+        return NGX_HTTP_UNKNOWN;
+        break;
+
+    case 7:
+        if (ngx_strncmp("OPTIONS", method_name->data, method_name->len) == 0) {
+            *method_name_ptr = &ngx_http_srcache_options_method;
+            return NGX_HTTP_OPTIONS;
+            break;
+        }
+        return NGX_HTTP_UNKNOWN;
+        break;
+
+    case 8:
+        if (ngx_strncmp("PROPFIND", method_name->data, method_name->len) == 0) {
+            *method_name_ptr = &ngx_http_srcache_propfind_method;
+            return NGX_HTTP_PROPFIND;
+            break;
+        }
+        return NGX_HTTP_UNKNOWN;
+        break;
+
+    case 9:
+        if (ngx_strncmp("PROPPATCH", method_name->data, method_name->len) == 0) {
+            *method_name_ptr = &ngx_http_srcache_proppatch_method;
+            return NGX_HTTP_PROPPATCH;
+            break;
+        }
+        return NGX_HTTP_UNKNOWN;
+        break;
+
+    default:
+        return NGX_HTTP_UNKNOWN;
+        break;
+    }
+
+    return NGX_HTTP_UNKNOWN;
+}
+
+
+ngx_int_t
+ngx_http_srcache_adjust_subrequest(ngx_http_request_t *sr,
+        ngx_http_srcache_parsed_request_t *parsed_sr)
+{
+    ngx_table_elt_t            *h;
+    ngx_http_core_main_conf_t  *cmcf;
+
+    sr->method = parsed_sr->method;
+    sr->method_name = parsed_sr->method_name;
+
+    sr->header_in = sr->parent->header_in;
+
+    /* we do not inherit the parent request's variables */
+    cmcf = ngx_http_get_module_main_conf(sr, ngx_http_core_module);
+    sr->variables = ngx_pcalloc(sr->pool, cmcf->variables.nelts
+                                        * sizeof(ngx_http_variable_value_t));
+
+    if (sr->variables == NULL) {
+        return NGX_HTTP_INTERNAL_SERVER_ERROR;
+    }
+
+    if (parsed_sr->content_length_n > 0) {
+        sr->headers_in.content_length_n = parsed_sr->content_length_n;
+        sr->request_body = parsed_sr->request_body;
+
+        sr->headers_in.content_length = ngx_pcalloc(sr->pool,
+                sizeof(ngx_table_elt_t));
+        sr->headers_in.content_length->value.data =
+            ngx_palloc(sr->pool, NGX_OFF_T_LEN);
+        if (sr->headers_in.content_length->value.data == NULL) {
+            return NGX_HTTP_INTERNAL_SERVER_ERROR;
+        }
+        sr->headers_in.content_length->value.len = ngx_sprintf(
+                sr->headers_in.content_length->value.data, "%O",
+                sr->headers_in.content_length_n) -
+                sr->headers_in.content_length->value.data;
+
+        if (ngx_list_init(&sr->headers_in.headers, sr->pool, 20,
+                    sizeof(ngx_table_elt_t)) != NGX_OK) {
+            return NGX_HTTP_INTERNAL_SERVER_ERROR;
+        }
+
+        h = ngx_list_push(&sr->headers_in.headers);
+        if (h == NULL) {
+            return NGX_HTTP_INTERNAL_SERVER_ERROR;
+        }
+
+        h->hash = sr->header_hash;
+
+        h->key = ngx_http_srcache_content_length_header_key;
+        h->value = sr->headers_in.content_length->value;
+
+        h->lowcase_key = ngx_pnalloc(sr->pool, h->key.len);
+        if (h->lowcase_key == NULL) {
+            return NGX_HTTP_INTERNAL_SERVER_ERROR;
+        }
+
+        ngx_strlow(h->lowcase_key, h->key.data, h->key.len);
+
+        dd("sr content length: %s", sr->headers_in.content_length->value.data);
+    }
+
+    return NGX_OK;
+}
+
---
+++ src/ngx_http_srcache_util.h
@@ -0,0 +1,75 @@
+#ifndef NGX_HTTP_SRCACHE_UTIL_H
+#define NGX_HTTP_SRCACHE_UTIL_H
+
+#include "ngx_http_srcache_filter_module.h"
+
+
+#define ngx_http_srcache_method_name(m) { sizeof(m) - 1, (u_char *) m " " }
+
+
+#ifndef ngx_str4cmp
+
+#  if (NGX_HAVE_LITTLE_ENDIAN && NGX_HAVE_NONALIGNED)
+
+#    define ngx_str4cmp(m, c0, c1, c2, c3)                                        \
+    *(uint32_t *) m == ((c3 << 24) | (c2 << 16) | (c1 << 8) | c0)
+
+#  else
+
+#    define ngx_str4cmp(m, c0, c1, c2, c3)                                        \
+    m[0] == c0 && m[1] == c1 && m[2] == c2 && m[3] == c3
+
+#  endif
+
+#endif /* ngx_str4cmp */
+
+#ifndef ngx_str3cmp
+
+#  define ngx_str3cmp(m, c0, c1, c2)                                       \
+    m[0] == c0 && m[1] == c1 && m[2] == c2
+
+#endif /* ngx_str3cmp */
+
+#ifndef ngx_str6cmp
+
+#  if (NGX_HAVE_LITTLE_ENDIAN && NGX_HAVE_NONALIGNED)
+
+#    define ngx_str6cmp(m, c0, c1, c2, c3, c4, c5)                                \
+    *(uint32_t *) m == ((c3 << 24) | (c2 << 16) | (c1 << 8) | c0)             \
+        && (((uint32_t *) m)[1] & 0xffff) == ((c5 << 8) | c4)
+
+#  else
+
+#    define ngx_str6cmp(m, c0, c1, c2, c3, c4, c5)                                \
+    m[0] == c0 && m[1] == c1 && m[2] == c2 && m[3] == c3                      \
+        && m[4] == c4 && m[5] == c5
+
+#  endif
+
+#endif /* ngx_str6cmp */
+
+ngx_str_t  ngx_http_srcache_content_length_header_key;
+
+ngx_str_t  ngx_http_srcache_get_method;
+ngx_str_t  ngx_http_srcache_put_method;
+ngx_str_t  ngx_http_srcache_post_method;
+ngx_str_t  ngx_http_srcache_head_method;
+ngx_str_t  ngx_http_srcache_copy_method;
+ngx_str_t  ngx_http_srcache_move_method;
+ngx_str_t  ngx_http_srcache_lock_method;
+ngx_str_t  ngx_http_srcache_mkcol_method;
+ngx_str_t  ngx_http_srcache_trace_method;
+ngx_str_t  ngx_http_srcache_delete_method;
+ngx_str_t  ngx_http_srcache_unlock_method;
+ngx_str_t  ngx_http_srcache_options_method;
+ngx_str_t  ngx_http_srcache_propfind_method;
+ngx_str_t  ngx_http_srcache_proppatch_method;
+
+ngx_int_t ngx_http_srcache_parse_method_name(ngx_str_t **method_name_ptr);
+void ngx_http_srcache_discard_bufs(ngx_pool_t *pool, ngx_chain_t *in);
+ngx_int_t ngx_http_srcache_adjust_subrequest(ngx_http_request_t *sr,
+        ngx_http_srcache_parsed_request_t *parsed_sr);
+
+
+#endif /* NGX_HTTP_SRCACHE_UTIL_H */
+
