a26bdc4f47a019fc83c7737ad131078624389360
agentzh (章亦春) <agentzh@gmail.com>
2010-07-07 14:00:16 +0800

    we no longer require ngx_postgres in build.sh.

--- util/build.sh
+++ util/build.sh
@@ -29,7 +29,6 @@ if [[ "$BUILD_CLEAN" -eq 1 || ! -f Makefile || "$root/config" -nt Makefile || "$
           --add-module=$root/../echo-nginx-module \
           --add-module=$root $opts \
           --add-module=$root/../rds-json-nginx-module \
-          --add-module=$root/../postgres-nginx-module \
           --add-module=$root/../drizzle-nginx-module \
           --add-module=$root/../memc-nginx-module \
           --with-debug
