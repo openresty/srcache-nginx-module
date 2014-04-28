2cf64204d4205a4b0b4486b1b1cc8c54501bebe2
Yichun Zhang (agentzh) <agentzh@gmail.com>
2014-04-27 21:59:22 -0700

    bugfix: we used to skip all the output header and body filters run before our filters (which unfortunately bypassed the standard ngx_not_modified module, for example). thanks Lloyd Zhou for the report in #29.

--- src/ngx_http_srcache_fetch.c
+++ src/ngx_http_srcache_fetch.c
@@ -18,11 +18,6 @@
 
 static ngx_int_t ngx_http_srcache_fetch_subrequest(ngx_http_request_t *r,
         ngx_http_srcache_loc_conf_t *conf, ngx_http_srcache_ctx_t *ctx);
-static ngx_int_t ngx_http_srcache_fetch_header_filter(ngx_http_request_t *r);
-static ngx_int_t ngx_http_srcache_test_not_modified(ngx_http_request_t *r);
-#if defined(nginx_version) && (nginx_version >= 9002)
-static ngx_int_t ngx_http_srcache_test_precondition(ngx_http_request_t *r);
-#endif
 static void ngx_http_srcache_post_read_body(ngx_http_request_t *r);
 
 
@@ -160,7 +155,7 @@ ngx_http_srcache_access_handler(ngx_http_request_t *r)
 
                 r->headers_out.content_length_n = len;
 
-                rc = ngx_http_srcache_fetch_header_filter(r);
+                rc = ngx_http_send_header(r);
 
                 dd("srcache fetch header returned %d", (int) rc);
 
@@ -175,15 +170,8 @@ ngx_http_srcache_access_handler(ngx_http_request_t *r)
 #endif
 
                 if (!r->filter_finalize) {
-                    rc = ngx_http_srcache_next_body_filter(r,
-                                                        ctx->body_from_cache);
-
-                    if (rc == NGX_ERROR) {
-                        r->connection->error = 1;
-                        return NGX_ERROR;
-                    }
-
-                    if (rc > NGX_OK) {
+                    rc = ngx_http_output_filter(r, ctx->body_from_cache);
+                    if (rc == NGX_ERROR || rc > NGX_OK) {
                         return rc;
                     }
                 }
@@ -444,95 +432,6 @@ ngx_http_srcache_fetch_subrequest(ngx_http_request_t *r,
 
     return NGX_OK;
 }
-
-
-static ngx_int_t
-ngx_http_srcache_fetch_header_filter(ngx_http_request_t *r)
-{
-    if (r->headers_out.status != NGX_HTTP_OK
-        || r->headers_out.last_modified_time == -1)
-    {
-        return ngx_http_srcache_next_header_filter(r);
-    }
-
-#if defined(nginx_version) && (nginx_version >= 9002)
-    if (r->headers_in.if_unmodified_since) {
-        return ngx_http_srcache_test_precondition(r);
-    }
-#endif
-
-    if (r->headers_in.if_modified_since) {
-        return ngx_http_srcache_test_not_modified(r);
-    }
-
-    return ngx_http_srcache_next_header_filter(r);
-}
-
-
-static ngx_int_t
-ngx_http_srcache_test_not_modified(ngx_http_request_t *r)
-{
-    time_t                     ims;
-    ngx_http_core_loc_conf_t  *clcf;
-
-    clcf = ngx_http_get_module_loc_conf(r, ngx_http_core_module);
-
-    if (clcf->if_modified_since == NGX_HTTP_IMS_OFF) {
-        return ngx_http_srcache_next_header_filter(r);
-    }
-
-    ims = ngx_http_parse_time(r->headers_in.if_modified_since->value.data,
-                              r->headers_in.if_modified_since->value.len);
-
-    ngx_log_debug2(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
-                   "srcache ims:%d lm:%d", ims,
-                   r->headers_out.last_modified_time);
-
-    if (ims != r->headers_out.last_modified_time) {
-
-        if (clcf->if_modified_since == NGX_HTTP_IMS_EXACT
-            || ims < r->headers_out.last_modified_time)
-        {
-            return ngx_http_srcache_next_header_filter(r);
-        }
-    }
-
-    r->headers_out.status = NGX_HTTP_NOT_MODIFIED;
-    r->headers_out.status_line.len = 0;
-    r->headers_out.content_type.len = 0;
-    ngx_http_clear_content_length(r);
-    ngx_http_clear_accept_ranges(r);
-
-    if (r->headers_out.content_encoding) {
-        r->headers_out.content_encoding->hash = 0;
-        r->headers_out.content_encoding = NULL;
-    }
-
-    return ngx_http_srcache_next_header_filter(r);
-}
-
-
-#if defined(nginx_version) && (nginx_version >= 9002)
-static ngx_int_t
-ngx_http_srcache_test_precondition(ngx_http_request_t *r)
-{
-    time_t  iums;
-
-    iums = ngx_http_parse_time(r->headers_in.if_unmodified_since->value.data,
-                               r->headers_in.if_unmodified_since->value.len);
-
-    ngx_log_debug2(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
-                   "srcache iums:%d lm:%d", iums,
-                   r->headers_out.last_modified_time);
-
-    if (iums >= r->headers_out.last_modified_time) {
-        return ngx_http_srcache_next_header_filter(r);
-    }
-
-    return ngx_http_filter_finalize_request(r, NULL,
-                                            NGX_HTTP_PRECONDITION_FAILED);
-}
-#endif
 
 
 static void
