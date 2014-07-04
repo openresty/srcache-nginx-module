b2b8dda5790a053a21f2e7b46b396ba1e7ae7c21
Yichun Zhang (agentzh) <agentzh@gmail.com>
2014-07-04 14:09:27 -0700

    removed the valgrind suppression rules for luajit because we no longer need it; also removed the hard-coded luajit path from RPATH in the developer build script.

--- util/build.sh
+++ util/build.sh
@@ -17,7 +17,7 @@ cd $root || exit 1
             #--without-http_memcached_module \
 ngx-build $force $version \
             --with-cc-opt="-O0" \
-            --with-ld-opt="-Wl,-rpath,$LUAJIT_LIB:/opt/postgres/lib:/opt/drizzle/lib:/usr/local/lib:/home/lz/lib" \
+            --with-ld-opt="-Wl,-rpath,/opt/postgres/lib:/opt/drizzle/lib:/usr/local/lib:/home/lz/lib" \
             --without-mail_pop3_module \
             --without-mail_imap_module \
             --without-mail_smtp_module \
