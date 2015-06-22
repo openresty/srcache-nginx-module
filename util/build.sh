41961728c6ebcd5ba8c116ab57b46ca62ffa42c5
Yichun Zhang (agentzh) <agentzh@gmail.com>
2015-06-22 21:35:24 +0800

    we disable ngx_ssi in our developer build script.

--- util/build.sh
+++ util/build.sh
@@ -45,11 +45,11 @@ ngx-build $force $version \
           --with-select_module \
           --with-poll_module \
           --with-rtsig_module \
+          --without-http_ssi_module \
           --with-debug
           #--add-module=/home/agentz/git/dodo/utils/dodo-hook \
           #--add-module=$home/work/ngx_http_auth_request-0.1 #\
           #--with-rtsig_module
           #--with-cc-opt="-g3 -O0"
           #--add-module=$root/../echo-nginx-module \
-  #--without-http_ssi_module  # we cannot disable ssi because echo_location_async depends on it (i dunno why?!)
 
